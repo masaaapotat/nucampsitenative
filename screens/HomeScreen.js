92d66f13a90c8e66dfb6f672c4a5764716d0842a
masaapotat <masaakavulanya@gmail.com>
2024-07-08 11:34:55 +0300

    workshop

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,42 @@
-import { Text, View } from 'react-native';
+import { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+import { Card } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
+import { PROMOTIONS } from "../shared/promotions";
+import { PARTNERS } from "../shared/partners";
 
-const HomeScreen = () => {
+const FeaturedItem = ({ item }) => {
+  if (item)
     return (
-        <View>
-            <Text>Home Screen</Text>
-        </View>
-    )
+      <Card containerStyle={{ padding: 0 }}>
+        <Card.Image source={item.image}>
+          <View style={{ justifyContent: "center", flex: 1 }}>
+            <Text style={{ color: "white", textAlign: "center", fontSize: 20 }}>
+              {item.name}
+            </Text>
+          </View>
+        </Card.Image>
+        <Text style={{ margin: 20 }}>{item.description}</Text>
+      </Card>
+    );
+
+  return <View />;
+};
+const HomeScreen = () => {
+  const [campsites, setCampsites] = useState(CAMPSITES);
+
+  const [promotions, setPromotions] = useState(PROMOTIONS);
+  const [partners, setPartners] = useState(PARTNERS);
+
+  const featCampsite = campsites.find((item) => item.featured);
+  const featPromotion = promotions.find((item) => item.featured);
+  const featPartners = partners.find((item) => item.featured);
+  return (
+    <ScrollView>
+     <FeaturedItem item={featCampsite} />
+     <FeaturedItem item={featPromotion} />
+     <FeaturedItem item={featPartners} />
+    </ScrollView>
+  );
 };
 export default HomeScreen;
