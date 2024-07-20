949cd0d0fdb6deb507f0d09f1eb3c58357ffa4cb
masaapotat <masaakavulanya@gmail.com>
2024-07-20 15:08:45 +0300

    Persist Redux Store

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import Main from './screens/MainComponents';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from './components/LoadingComponent';
 
 export default function App() {
     return (
         <Provider store={store}>
+            <PersistGate persistor={persistor} loading={<Loading />} >
             <NavigationContainer>
                 <Main />
             </NavigationContainer>
+            </PersistGate>
         </Provider>
     );
   //   navigation container is used to manage our navigation and must wrap all our components using navigation
