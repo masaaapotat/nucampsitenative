f81617e567ec0b6dda6b0d8620cd64295af260d3
masaapotat <masaakavulanya@gmail.com>
2024-07-25 12:59:30 +0300

    Register Form

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -110,74 +110,173 @@ const LoginTab = ({ navigation }) => {
 };
 
 const RegisterTab = () => {
-  return <ScrollView></ScrollView>;
+  // Render the register form
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [remember, setRemember] = useState(false);
+
+  const handleRegister = () => {
+    const userInfo = {
+      username,
+      password,
+      firstName,
+      lastName,
+      email,
+      remember,
+    };
+
+    console.log(JSON.stringify("User info:", userInfo));
+    if (remember) {
+      // Save user information securely if "Remember Me" is checked
+      SecureStore.setItemAsync(
+        "userinfo",
+        JSON.stringify({
+          username,
+          password,
+        })
+      ).catch((error) => console.log("Could not save user info", error));
+    } else {
+      // Delete stored user information if "Remember Me" is unchecked
+      SecureStore.deleteItemAsync("userinfo").catch((error) =>
+        console.log("Could not delete user info", error)
+      );
+    }
+  };
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Input
+          placeholder="Username"
+          leftIcon={{ type: "font-awesome", name: "user-o" }}
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder="Password"
+          leftIcon={{ type: "font-awesome", name: "key" }}
+          value={password}
+          onChangeText={(password) => setPassword(password)}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder="First Name"
+          leftIcon={{ type: "font-awesome", name: "user-o" }}
+          value={firstName}
+          onChangeText={(firstName) => setFirstName(firstName)}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder="Last Name"
+          leftIcon={{ type: "font-awesome", name: "user-o" }}
+          value={lastName}
+          onChangeText={(lastName) => setLastName(lastName)}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <Input
+          placeholder="Email"
+          leftIcon={{ type: "font-awesome", name: "envelope-o" }}
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+          containerStyle={styles.formInput}
+          leftIconContainerStyle={styles.formIcon}
+        />
+        <CheckBox
+          title="Remember Me"
+          checked={remember}
+          onPress={() => setRemember(!remember)}
+          containerStyle={styles.formCheckbox}
+        />
+        <View style={styles.formButton}>
+          <Button
+            color="#5637DD"
+            title="Register"
+            onPress={handleRegister}
+            icon={
+              <Icon
+                name="user-plus"
+                type="font-awesome"
+                color="#fff"
+                iconStyle={{ marginRight: 10 }}
+              />
+            }
+            buttonStyle={{ backgroundColor: "#5637DD" }}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 const Tab = createBottomTabNavigator();
 
 const LoginScreen = () => {
-    const tabBarOptions = {
-        activeBackgroundColor: '#5637DD',
-        inactiveBackgroundColor: '#CEC8FF',
-        activeTintColor: '#fff',
-        inactiveTintColor: '#808080',
-        labelStyle: { fontSize: 16 }
-    };
+  const tabBarOptions = {
+    activeBackgroundColor: "#5637DD",
+    inactiveBackgroundColor: "#CEC8FF",
+    activeTintColor: "#fff",
+    inactiveTintColor: "#808080",
+    labelStyle: { fontSize: 16 },
+  };
 
-    return (
-        <Tab.Navigator tabBarOptions={tabBarOptions}>
-            <Tab.Screen
-                name='Login'
-                component={LoginTab}
-                options={{
-                    tabBarIcon: (props) => {
-                        return (
-                            <Icon
-                                name='sign-in'
-                                type='font-awesome'
-                                color={props.color}
-                            />
-                        );
-                    }
-                }}
-            />
-            <Tab.Screen
-                name='Register'
-                component={RegisterTab}
-                options={{
-                    tabBarIcon: (props) => {
-                        return (
-                            <Icon
-                                name='user-plus'
-                                type='font-awesome'
-                                color={props.color}
-                            />
-                        );
-                    }
-                }}
-            />
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator tabBarOptions={tabBarOptions}>
+      <Tab.Screen
+        name="Login"
+        component={LoginTab}
+        options={{
+          tabBarIcon: (props) => {
+            return (
+              <Icon name="sign-in" type="font-awesome" color={props.color} />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Register"
+        component={RegisterTab}
+        options={{
+          tabBarIcon: (props) => {
+            return (
+              <Icon name="user-plus" type="font-awesome" color={props.color} />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
 };
 
 // Styles for the LoginScreen component
 const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
-    margin: 20,
-  },
-  formInput: {
-    margin: 20,
-  },
-  formCheckbox: {
-    margin: 20,
-    backgroundColor: null,
+    margin: 10,
   },
   formIcon: {
     marginRight: 10,
   },
-  formButton: {
-    margin: 60,
+  formInput: {
+    padding: 8,
+    height: 60,
   },
+  formCheckbox: {
+    margin: 8,
+    backgroundColor: null,
+  },
+  formButton: {
+    margin: 20,
+    marginRight: 40,
+    marginLeft: 40,
+  },
+ 
+  
 });
 
 export default LoginScreen;
